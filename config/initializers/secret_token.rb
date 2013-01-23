bc79f02956cc3298d64c1cbb71aad7216b97b2c0
machipeddy9@gmail.com <om@om-G31M-ES2L.(none)>
2013-01-23 13:31:53 +0530

    Generated the rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticket::Application.config.secret_token = '8aec3f180da07a48229a233d45e6ff30689e9f5a2328539877656e72971967f5c675c701b77d6b75aa2da70e15c0d69c4eb27d16647d600ad26e182ee4bbd252'
